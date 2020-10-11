9e03d8255255d6bacc4037b152f649d55c10269d
unknown <karun.r.gowda@gmail.com>
2020-10-11 06:45:12 +0200

    Included code to include '/user/signin' endpoint
    Couldnt test the endpoint as there is database connection issue.

--- Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -4,10 +4,13 @@ import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
+
+import java.time.ZonedDateTime;
 
 @Service
 public class UserBusinessService {
@@ -32,6 +35,36 @@ public class UserBusinessService {
         } else {
             throw new AuthenticationFailedException("SGR-002", "This user has already been registered, try with any other emailId");
         }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthTokenEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
+        UserEntity userEntity = userDao.getUserByEmail(username);
+        if(userEntity == null){
+            throw new AuthenticationFailedException("ATH-001", "This username does not exist");
+        }
+
+        final String encryptedPassword = passwordCryptographyProvider.encrypt(password, userEntity.getSalt());
+        if(encryptedPassword.equals(userEntity.getPassword())){
+            JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+            UserAuthTokenEntity userAuthToken = new UserAuthTokenEntity();
+            userAuthToken.setUser(userEntity);
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime expiresAt = now.plusHours(8);
+            userAuthToken.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+            userAuthToken.setLoginAt(now);
+            userAuthToken.setExpiresAt(expiresAt);
+            //userAuthToken.setCreatedBy("api-backend");
+            //userAuthToken.setCreatedAt(now);
+            userDao.createAuthToken(userAuthToken);
+            userDao.updateUser(userEntity);
+            //userEntity.setLastLoginAt(now);
+            return userAuthToken;
+        }
+        else{
+            throw new AuthenticationFailedException("ATH-002", "Password failed");
+        }
+
     }
 }
 
