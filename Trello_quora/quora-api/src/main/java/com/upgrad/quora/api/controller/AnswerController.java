640b5866aa94e0d50b109bb2c75af8123b33d0a8
KarunRamesh <karun.r.gowda@gmail.com>
2020-10-18 06:57:22 +0200

    Controller, BusinessService and Dao classes are created for Answer module.

---
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -0,0 +1,26 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.AnswerRequest;
+import com.upgrad.quora.api.model.SigninResponse;
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.UserBusinessService;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/")
+public class AnswerController {
+    @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> createAnswer(@PathVariable("questionId") final String questionId, final AnswerRequest answerRequest) throws AuthenticationFailedException {
+        
+    }
+}
---
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -0,0 +1,6 @@
+package com.upgrad.quora.service.dao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AnswerDao {
+}
